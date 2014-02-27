83b067441d03bd01a7a9c8e0538ca14b23dc4efe
Kuangyu Wang <wangw1023@gmail.com>
2014-02-27 16:27:24 -0500

    test echo output files

--- hpc/predAAprob_allProteins/runPredAAprob_allProteins.sh
+++ hpc/predAAprob_allProteins/runPredAAprob_allProteins.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+resultPath=/home/kwang2/result/aaProb_allProteins/
 
 for f in /home/kwang2/data/newData_Feb3_pwNACCESS_noMismatch/*.pw; do
 	#
 	#sbatch -o $f.slurmlogp6 find.p6.stupidscript.sh $f &
-	echo $f
+	resultFile=$resultPath$(f%.pw)".csv"
 done

72af9b7914f6473e21149a107c7a83b0ee192e5f
Kuangyu Wang <kwang2@captainkirk.statgen.ncsu.edu>
2014-02-27 23:20:31 -0500

    fixing predAAprob_all...

--- hpc/predAAprob_allProteins/runPredAAprob_allProteins_firstOrder.sh
+++ hpc/predAAprob_allProteins/runPredAAprob_allProteins_firstOrder.sh
@@ -3,6 +3,8 @@ resultPath=/home/kwang2/result/aaProb_allProteins_firstOrder/
 
 for f in /home/kwang2/data/newData_Feb3_pwNACCESS_noMismatch/*.pw; do
 	#sbatch -o $f.slurmlogp6 find.p6.stupidscript.sh $f &
-	resultFile=$resultPath${f%.pw}".csv"
-	#sbatch -o $f.slurmlog  $f $resultFile
+        fname=$(basename $f)
+	resultFile=$resultPath${fname%.pw}".csv"
+	#echo $resultFile
+	sbatch -o $fname.slurmlog predAAprob_allProteins_firstOrder.sh $f $resultFile  &
 done

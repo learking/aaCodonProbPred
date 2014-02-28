deef60928a60fee888ef70db64363718265dbd51
Kuangyu Wang <wangw1023@gmail.com>
2014-02-28 13:11:41 -0500

    finished computing ranks

---
+++ getRank/getRank_allProteins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+resultPath=/media/work/newData_Feb3_2014/aaProb_allProteins_rank/
+
+for f in /media/work/newData_Feb3_2014/aaProb_allProteins/*.csv; do
+        fname=$(basename $f)
+	resultFile=$resultPath${fname%.csv}"_rank.txt"
+	python /home/kuangyu/workspace/aaCodonProbPred/getRank/rankOutput.py $f $resultFile
+	#echo $resultFile
+done
---
+++ getRank/rankOutput.py
@@ -0,0 +1,22 @@
+import sys
+
+def getRank(inputFile, outputFile):
+    fInput = open(inputFile, "r")
+    allInput = fInput.readlines()
+    fInput.close()
+
+    fOutput = open(outputFile, "w")
+    for line in allInput:
+        tmpLine = line.rstrip().split(",")
+        trueProb = tmpLine[4]
+        sortedProbs = sorted(map(float, tmpLine[5:len(tmpLine)]), reverse=True)
+        rank = map(str,sortedProbs).index(trueProb) + 1
+        resultLine = ",".join(tmpLine[0:5]) + "," + str(rank) + "\n"
+        fOutput.write(resultLine)
+        #print rank
+    fOutput.close()
+
+if  __name__ =='__main__':
+    inputFile = sys.argv[1]
+    outputFile = sys.argv[2]
+    getRank(inputFile, outputFile)

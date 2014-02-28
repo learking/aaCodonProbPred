20a073ee48f3200fc5eca58965f62e5284d17b0a
Kuangyu Wang <wangw1023@gmail.com>
2014-02-28 11:59:42 -0500

    study hydro plots finished

--- studyHydroResult/plot_sa_vs_prob.r
+++ studyHydroResult/plot_sa_vs_prob.r
@@ -1,2 +1,13 @@
 #read in csv and set column names for it
+library(data.table)
+data = fread('/home/kuangyu/workspace/aaCodonProbPred/studyHydroResult/aaProb_sa_0_172.csv')
+setnames(data, c('A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y'))
 
+x = seq(1,172,2)
+
+for(i in names(data)){
+	tmpFname = paste0("/home/kuangyu/workspace/aaCodonProbPred/studyHydroResult/fig/",i,"_sanac.png")
+	png(filename=tmpFname)
+	plot(x,data[,get(i)],xlab=i)
+	dev.off()
+}

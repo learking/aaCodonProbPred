b59c737a7dbcc75a96d9b89906f42b6d6d5df9db
Kuangyu Wang <kwang2@captainkirk.statgen.ncsu.edu>
2014-03-01 16:05:40 -0500

    TD13

--- hpc/predAAprob_allProteins_TD13/run_all_TD13.sh
+++ hpc/predAAprob_allProteins_TD13/run_all_TD13.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-resultPath=/home/kwang2/result/aaProb_allProteins_firstOrder/
+resultPath=/home/kwang2/result/aaProb_allProteins_TD13/
 
 for f in /home/kwang2/data/newData_Feb3_pwNACCESS_noMismatch/*.pw; do
 	#sbatch -o $f.slurmlogp6 find.p6.stupidscript.sh $f &
         fname=$(basename $f)
 	resultFile=$resultPath${fname%.pw}".csv"
 	#echo $resultFile
-	sbatch -o $f.slurmlog predAAprob_allProteins_firstOrder.sh $f $resultFile  &
+	python /home/kwang2/scripts/aaCodonProbPred/predAAprob/predAAprob_hpc_TD13.py $f $resultFile
 done
--- hpc/predAAprob_allProteins_firstOrder/predAAprob_allProteins_firstOrder.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash/
-python /home/kwang2/scripts/aaCodonProbPred/predAAprob/predAAprob_firstOrder_hpc.py $1 $2
